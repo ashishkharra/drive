236d91c47afbc9a11cd77f02617cca86aeb89683
ashishkharra <ashishkharra6@gmail.com>
2025-03-28 22:09:20 +0530

    index.js

--- APIs/index.js
+++ APIs/index.js
@@ -13,21 +13,23 @@ const app = express();
 const isProduction = process.env.NODE_ENV === 'production';
 const uri = process.env.MONGO_URI;
 
+// Replace existing CSP middleware with this:
 app.use((req, res, next) => {
     res.setHeader(
-        "Content-Security-Policy",
-        [
-            "default-src 'self'",
-            "script-src 'self' https://www.gstatic.com https://apis.google.com 'unsafe-inline' 'unsafe-eval' blob: data:",
-            "frame-src 'self' https://www.gstatic.com https://accounts.google.com",
-            "style-src 'self' https://www.gstatic.com 'unsafe-inline'",
-            "connect-src 'self' https://www.googleapis.com https://*.googleapis.com",
-            "img-src 'self' https://www.gstatic.com data:",
-            "font-src 'self' https://www.gstatic.com data:"
-        ].join("; ")
+      "Content-Security-Policy",
+      [
+        "default-src 'self'",
+        "script-src 'self' https://www.gstatic.com https://ssl.gstatic.com https://apis.google.com 'unsafe-inline' 'unsafe-eval' blob: data:",
+        "script-src-elem 'self' https://www.gstatic.com https://ssl.gstatic.com https://apis.google.com 'unsafe-inline'",
+        "frame-src 'self' https://www.gstatic.com https://accounts.google.com",
+        "style-src 'self' https://www.gstatic.com 'unsafe-inline'",
+        "connect-src 'self' https://www.googleapis.com https://*.googleapis.com",
+        "img-src 'self' https://www.gstatic.com data:",
+        "font-src 'self' https://www.gstatic.com data:"
+      ].join("; ")
     );
     next();
-});
+  });
 app.use(express.json());
 app.use(cors({
     origin: 'https://drive-hxq7.vercel.app',  // Specify the frontend's origin
