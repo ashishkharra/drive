2df89c6c77523e31f34f97341720026ef1b9e7d9
ashishkharra <ashishkharra6@gmail.com>
2025-03-28 19:40:35 +0530

    vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -17,8 +17,8 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      '/api': {
-        target: process.env.VITE_BACKEND_URL || 'http://localhost:3000',
+      '/': {
+        target: process.env.VITE_BACKEND_PORT || 'http://localhost:3000',
         changeOrigin: true,
         secure: false,
       }
